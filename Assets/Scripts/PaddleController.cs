e18b0f7819656274c42ce9330b5d9e4d39d889c2
Oscar Olarte <oscar.rpr@gmail.com>
2014-05-23 23:25:03 -0500

    Repository initialization with basic project setup 1. Basic movement with arrow keys for the paddle and added some test resources

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float edgeScreenOffset;
+	
+	private Vector3 moveDirection;
+
+	private bool isStopped;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPosition = transform.position;
+		float speed = moveSpeed;
+		if( Input.GetKeyDown(KeyCode.RightArrow) ) {
+			moveDirection = transform.right;
+			moveDirection.Normalize();
+		} else if( Input.GetKeyDown(KeyCode.LeftArrow) ) {
+			moveDirection = -transform.right;
+			moveDirection.Normalize();
+		}
+
+		Vector3 target = moveDirection * moveSpeed + currentPosition;
+		transform.position = Vector3.Lerp( currentPosition, target, Time.deltaTime );
+
+		enforceBounds();
+	}
+
+	private void enforceBounds()
+	{
+		Vector3 newPosition = transform.position; 
+		Camera mainCamera = Camera.main;
+		Vector3 cameraPosition = mainCamera.transform.position;
+
+		float xDist = mainCamera.aspect * mainCamera.orthographicSize * edgeScreenOffset; 
+		float xMax = cameraPosition.x + xDist;
+		float xMin = cameraPosition.x - xDist;
+
+		if ( newPosition.x < xMin || newPosition.x > xMax ) {
+			newPosition.x = Mathf.Clamp( newPosition.x, xMin, xMax );
+			//moveDirection.x = -moveDirection.x;
+		}
+
+		transform.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/ScreenRelativePosition.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenRelativePosition : MonoBehaviour {
+
+	public enum ScreenEdge {LEFT, RIGHT, TOP, BOTTOM};
+	public ScreenEdge screenEdge;
+	public float yOffset;
+	public float xOffset;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 newPosition = transform.position;
+		Camera camera = Camera.main;
+
+		switch(screenEdge)
+		{
+		case ScreenEdge.LEFT:
+			newPosition.x = -camera.aspect * camera.orthographicSize + xOffset;
+			newPosition.y = yOffset;
+			break;
+		case ScreenEdge.RIGHT:
+			newPosition.x = camera.aspect * camera.orthographicSize + xOffset;
+			newPosition.y = yOffset;
+			break;
+		case ScreenEdge.TOP:
+			newPosition.y = camera.orthographicSize + yOffset;
+			newPosition.x = xOffset;
+			break;
+		case ScreenEdge.BOTTOM:
+			newPosition.y = -camera.orthographicSize + yOffset;
+			newPosition.x = xOffset;
+			break;
+		}
+		transform.position = newPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
