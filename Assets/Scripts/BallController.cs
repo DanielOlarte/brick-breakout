41525b85a62c49c8f4a73c51b99336bc1fa86b78
jsrc1115 <jsrc1987@gmail.com>
2014-05-24 12:13:33 -0500

    Se le agrega a la bola movimiento y colision con los bordes de la camara.

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public Vector2 speed = new Vector2(0.5f,-1f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(speed * Time.deltaTime);
+		checkBoundaries ();
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Vector2 normal = collision.contacts [0].normal;
+		if ( Mathf.Approximately( Mathf.Abs(normal.x), 1.0f) ){
+			speed.x = speed.x * -1;
+		}
+		if ( Mathf.Approximately( Mathf.Abs(normal.y) , 1.0f)) {
+			speed.y = speed.y * -1;
+		}
+	}
+
+	void checkBoundaries()
+	{
+		Vector3 newPosition = transform.position; 
+		Camera mainCamera = Camera.main;
+		Vector3 cameraPosition = mainCamera.transform.position;
+
+		Vector3 colliderSize = gameObject.renderer.bounds.extents;
+		float xDist = mainCamera.aspect * mainCamera.orthographicSize; 
+		float xMax = cameraPosition.x + xDist - colliderSize.x;
+		float xMin = cameraPosition.x - xDist + colliderSize.x;
+
+		if ( newPosition.x < xMin || newPosition.x > xMax ) {
+			newPosition.x = Mathf.Clamp( newPosition.x, xMin, xMax );
+			speed.x = speed.x * -1;
+			Debug.Log("X Boundary");
+		}
+
+		float yDist = mainCamera.orthographicSize; 
+
+		float yMax = cameraPosition.y + yDist - colliderSize.y;
+
+		if ( newPosition.y > yMax ) {
+			newPosition.y = Mathf.Clamp( newPosition.y, float.MinValue, yMax );
+			speed.y = speed.y * -1;
+			Debug.Log("Y Boundary");
+		}
+
+		transform.position = newPosition;
+	}
+}
