cffb42c620870f7b652cb42f8d261a4e654e0629
Oscar Olarte <oscar.rpr@gmail.com>
2014-08-16 14:03:22 -0500

    1. Fix multiple balls objects effect to send balls in the upper degrees only

--- Assets/Scripts/Bricks/PowerUps/MultipleBallsObj.cs
+++ Assets/Scripts/Bricks/PowerUps/MultipleBallsObj.cs
@@ -5,7 +5,9 @@ using System.Collections.Generic;
 public class MultipleBallsObj : MonoBehaviour {
 
 	public int numberBalls = 3;
-	public float powerModifier = 20.0f;
+	public float powerModifier = 10.0f;
+	public float limitLowAngle = 40.0f;
+	public float limitHighAngle = 140.0f;
 	public float scoreModifier = 2.0f;
 	
 	private string modifierStr = ScoreUtils.MULTIPLE_BALLS_MODIFIER;
@@ -31,8 +33,11 @@ public class MultipleBallsObj : MonoBehaviour {
 					ballController.setOnPaddle(false);
 					Vector2 velocity = gameManager.getSpeedBall();
 					ball.rigidbody2D.velocity = velocity;
-					float randomModifierForce = Random.Range (-powerModifier, powerModifier);
-					ball.rigidbody2D.AddForce( transform.right * randomModifierForce, ForceMode2D.Impulse );
+
+					float randomAngle = Random.Range (limitLowAngle, limitHighAngle);
+					Vector3 dir = Quaternion.AngleAxis(randomAngle, Vector3.forward) * Vector3.right;
+					ball.rigidbody2D.AddForce(dir*powerModifier, ForceMode2D.Impulse);
+
 					gameManager.addBall(ball);
 				}
 
